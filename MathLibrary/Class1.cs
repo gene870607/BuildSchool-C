cd7f354417684eb32a240afb80354b7f7d4af7a6
mizugakun@gmail.com <mizugakun@gmail.com>
2020-03-16 15:10:31 +0800

    remove divide operation add getQuitient add gerReminder

--- MathLibrary/Class1.cs
+++ MathLibrary/Class1.cs
@@ -12,7 +12,7 @@ namespace MathLibrary
         {
             return a + b;
         }
-
+        
         public static decimal Minus(this decimal a, decimal b)
         {
             return a - b;
@@ -23,9 +23,29 @@ namespace MathLibrary
             return a * b;
         }
 
-        public static decimal Divide(this decimal a, decimal b)
+        public static decimal getQuotient(this decimal a, decimal b)
         {
+            if (a == 0) throw new ArgumentException(); 
             return a / b;
         }
+
+        public static decimal getReminder(this decimal a, decimal b)
+        {
+            if (a == 0) throw new ArgumentException();
+            bool negative = false;
+            if (a > 0 ^ b > 0) negative = true;
+            if (a < 0) a *= -1;
+            if (b < 0) b *= -1;
+            decimal ans = 0;
+
+            while(a - b > 0)
+            {
+                a -= b;
+                ans++;
+            }
+            return (negative)? ans * -1  : ans;
+        }
+
+
     }
 }
