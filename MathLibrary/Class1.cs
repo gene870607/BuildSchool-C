12566e2f03b1515bad4a042b6580a14f2103d76f
mizugakun@gmail.com <mizugakun@gmail.com>
2020-03-16 15:53:30 +0800

    add operation function

--- MathLibrary/Class1.cs
+++ MathLibrary/Class1.cs
@@ -23,29 +23,81 @@ namespace MathLibrary
             return a * b;
         }
 
-        public static decimal getQuotient(this decimal a, decimal b)
+        public static decimal GetQuotient(this decimal a, decimal b)
         {
             if (a == 0) throw new ArgumentException(); 
             return a / b;
         }
 
-        public static decimal getReminder(this decimal a, decimal b)
+        public static decimal GetPercentage(this decimal a)
         {
-            if (a == 0) throw new ArgumentException();
-            bool negative = false;
-            if (a > 0 ^ b > 0) negative = true;
-            if (a < 0) a *= -1;
-            if (b < 0) b *= -1;
-            decimal ans = 0;
-
-            while(a - b > 0)
-            {
-                a -= b;
-                ans++;
-            }
-            return (negative)? ans * -1  : ans;
+            return a / 100;
         }
 
+        public static decimal GetPI()
+        {
+            return (decimal)Math.PI;
+        }
 
+        public static decimal GetE()
+        {
+            return (decimal)Math.E;
+        }
+
+        public static decimal GetTenExp(this decimal a, decimal b)
+        {
+            return a * (decimal)Math.Pow(10, decimal.ToDouble(b));
+        }
+
+        public static decimal Pow(this decimal a, decimal b)
+        {
+            double _a = decimal.ToDouble(a);
+            double _b = decimal.ToDouble(b);
+            return (decimal)Math.Pow(_a, _b);
+        }
+
+        public static decimal GetFactorial(this decimal a)
+        {
+            if (a < 0) throw new ArgumentException();
+            if (a == 0) return 1;
+            decimal _a = decimal.ToInt32(a);
+            decimal ans = 1;
+            while(_a > 1)
+            {
+                ans *= _a;
+                _a--;
+            }
+            return ans;
+        }
+
+        public static decimal GetSin(this decimal a)
+        {
+            return (decimal)Math.Sin(decimal.ToDouble(a));
+        }
+
+        public static decimal GetCos(this decimal a)
+        {
+            return (decimal)Math.Cos(decimal.ToDouble(a));
+        }
+
+        public static decimal GetTan(this decimal a)
+        {
+            return (decimal)Math.Tan(decimal.ToDouble(a));
+        }
+
+        public static decimal GetLog(this decimal a)
+        {
+            return (decimal)Math.Log10(decimal.ToDouble(a));
+        }
+
+        public static decimal GetLn(this decimal a)
+        {
+            return (decimal)Math.Log(decimal.ToDouble(a));
+        }
+
+        public static decimal GetRoot(this decimal a)
+        {
+            return (decimal)Math.Sqrt(decimal.ToDouble(a));
+        }
     }
 }
