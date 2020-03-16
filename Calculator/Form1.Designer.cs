ec07300f8ae07621bc67a564175346f072367d11
俊佑 郭 <geneissmart@gmail.com>
2020-03-16 16:22:09 +0800

    刪除form_

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -409,7 +409,6 @@
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
